985d58f531632bb6c8d64d7aed26bfb0310cdd24
Pedro Henrique <pe.henri.2005@gmail.com>
2026-02-24 22:13:42 -0300

    Conteudo-Aula1-JavaScript

---
+++ Aula 1/src/script.js
@@ -0,0 +1,11 @@
+// Exemplos de variável let
+let nome = 'Pedro'; 
+let nome2 = 'Sophia Liz'; 
+
+console.log(nome);
+console.log(nome2)
+
+// Exemplos de varíavel const
+const times = ['Corinthians', 'Real Madrid', 'Barcelona'];
+
+console.log(times);
