029d3d70c80608382478ced4c2fed86d4bece276
Pedro Henrique <pe.henri.2005@gmail.com>
2026-03-02 19:46:39 -0300

    Exemplo Calculadora com Switch

---
+++ Aula 4/src/script.js
@@ -0,0 +1,37 @@
+// Calculadora com Switch
+let numero1 = parseFloat(prompt("Informe o primeiro valor: "));
+let numero2 = parseFloat(prompt("Informe o segundo valor: "));
+
+let opcoes = prompt(
+    "Selecione a operação: \n \n" +
+    "+: Adição1 \n" +
+    "-: Subtração1 \n" +
+    "*: Multiplicação \n" +
+    "/: Divisão1 \n"
+);
+
+switch (opcoes) {
+  case "+":
+    alert("O resultado da operação é: " + (numero1 + numero2));
+    break;
+
+  case "-":
+    alert("O resultado da operação é: " + (numero1 - numero2));
+    break;
+
+  case "*":
+    alert("O resultado da operação é: " + (numero1 * numero2));
+    break;
+
+  case "/":
+    if (numero2 === 0) {
+        alert("Não é possivel dividir por zero. Tente Novamente!");
+    } else {
+        alert("O resultado da operação é: " + (numero1 / numero2));
+    }
+    break
+
+default:
+    alert("Informe um valor válido!")
+    window.location.reload();
+}
