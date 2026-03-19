817e4ddf26fcfee6f923d2e176e9bcbaf82e8a8c
Pedro Henrique <pe.henri.2005@gmail.com>
2026-03-18 22:12:30 -0300

    Aula 14 - Estudando Manipulação de Elementos no HTML

---
+++ Aula 14/src/script03.js
@@ -0,0 +1,8 @@
+// Utilizando o InnerHTML
+const elemento1 = document.querySelector(".mudandoElemento");
+
+elemento1. innerHTML = "Somente o trauma coletivo gera a união.";
+
+// Alterando atributos
+const elemento = document.querySelector(".foto");
+elemento.scr = "tigrinho.png"
